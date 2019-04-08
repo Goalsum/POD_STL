9f40999770b0c6aac6fd58105f4f6ab1f1982c2d
Goalsum <goalsum@qq.com>
2019-04-08 20:43:45 +0800

    Update _operator.h

--- headers/_operator.h
+++ headers/_operator.h
@@ -1,5 +1,11 @@
-// ���ļ����ڲ�ͷ�ļ�(�ļ������»��߿�ͷ)���������ⲿʹ����ֱ�Ӱ�����
-// ���ļ�����һЩ����ͨ�ò��������������κ����͡�
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2015-2019. All rights reserved.
+ * Description: 本文件定义一些公用通用操作符，适用于任何类型。
+ * Author: 高尚 gaoshang@huawei.com
+ * Create: 2015-04-07
+ */
+
+// 本文件是内部头文件(文件名以下划线开头)，不允许外部使用者直接包含。
 
 #ifndef _PODSTL_OPERATOR_H
 #define _PODSTL_OPERATOR_H
