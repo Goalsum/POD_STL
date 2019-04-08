b1623f97f783eda60f9f446c26fd59f85aea92aa
Goalsum <goalsum@qq.com>
2019-04-08 20:46:08 +0800

    Update _uninitialized.h

--- headers/_uninitialized.h
+++ headers/_uninitialized.h
@@ -1,5 +1,11 @@
-// ���ļ����ڲ�ͷ�ļ�(�ļ������»��߿�ͷ)���������ⲿʹ����ֱ�Ӱ�����
-// ���ļ�������һЩ���ó�ʼ���㷨��
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2015-2019. All rights reserved.
+ * Description: 本文件定义了一些公用初始化算法。
+ * Author: 高尚 gaoshang@huawei.com
+ * Create: 2015-04-07
+ */
+
+// 本文件是内部头文件(文件名以下划线开头)，不允许外部使用者直接包含。
 
 #ifndef _PODSTL_UNINITIALIZED_H
 #define _PODSTL_UNINITIALIZED_H
@@ -10,7 +16,7 @@
 namespace podstl
 {
 
-// �����ڲ���ʼ��ʱʹ�ã�ֻ�����POD���ͳ�ʼ���������ڴ濽����ʽ�����������������ָ�룡
+// 仅仅内部初始化时使用，只能针对POD类型初始化，采用内存拷贝方式。传入迭代器必须是指针！
 template <typename _Size, typename _Tp>
 inline _Tp* _uinit_aux_aux(_Tp* __first, _Size __n, const _Tp& __val, const _FalseType& /* HasDefaultZero */)
 {
